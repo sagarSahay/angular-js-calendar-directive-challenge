d7bc9a23849ab7973f0ae3c8aa5b0492f9687e23
Sagar <sag.sahay@gmail.com>
2015-08-04 23:58:50 +0100

    Adding directives

--- app/app.js
+++ app/app.js
@@ -1,3 +1,50 @@
-angular.module('calendarDemoApp', []);
+var app = angular.module('calendarDemoApp', []);
+
+app.controller("calendarController",["CalendarRange",function($scope,CalendarRange){
+    var vm=this;
+}]);
+
+app.directive("dateControl", function ($sce) {
+    return {
+        restrict: 'E',
+        scope: true,
+        require: "calendarController",
+        templateUrl: 'date-control.html',
+        controllerAs: 'ctrl',
+        controller: function ($scope, $element, $attrs) {
+            var vm = this;
+            vm.years=[];
+            vm.months=[{month:"January",value:1},
+                {month:"February",value:2},
+                {month:"March",value:3},
+                {month:"April",value:4},
+                {month:"May",value:5},
+                {month:"June", value:6},
+                {month:"July", value:7},
+                {month:"August",  value:8} ,
+                {month:"September",  value:9},
+                {month:"October",  value:10},
+                {month:"November",  value:11},
+                {month:"December", value:12}];
+
+            for(i=1900;i<=2020;i++){
+                vm.years.push(i);
+            }
+
+            vm.changeMonth=function(month){
+                console.log("month ->"+month);
+            };
+
+            vm.changeYear=function(year){
+              console.log("year -> "+year);
+            };
+
+        },
+        link: function (scope, element, attrs) {
+
+        },
+    };
+});
+
 
 // your controller and directive code go here
